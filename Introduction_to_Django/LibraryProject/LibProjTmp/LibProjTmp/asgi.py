5eb5284cc5e4821ff514e40fddb3b9010303ed3b
Moana <missn.hadebe@gmail.com>
2026-01-10 12:04:47 +0200

    Add Django LibraryProject with README, manage.py, and settings.py

---
+++ Introduction_to_Django/LibraryProject/LibProjTmp/LibProjTmp/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for LibProjTmp project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LibProjTmp.settings')
+
+application = get_asgi_application()
