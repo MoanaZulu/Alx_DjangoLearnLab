2cefac69a2213f428c5b5cec0f8f083417506d5b
Moana <missn.hadebe@gmail.com>
2026-01-17 23:06:14 +0200

    Add list_books function view, LibraryDetailView class view, templates, and URL routing

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -6,3 +6,13 @@ urlpatterns = [
     path('books/<int:book_id>/edit/', views.edit_book, name='edit_book'),
     path('books/<int:book_id>/delete/', views.delete_book, name='delete_book'),
 ]
+
+
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('books/', views.list_books, name='list_books'),
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -14,3 +14,21 @@ def edit_book(request, book_id): ...
 
 @permission_required('relationship_app.can_delete_book')
 def delete_book(request, book_id): ...
+
+
+
+
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import DetailView
+from .models import Book, Library
+
+# Function-based view: list all books
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+
+# Class-based view: show details of a specific library
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
