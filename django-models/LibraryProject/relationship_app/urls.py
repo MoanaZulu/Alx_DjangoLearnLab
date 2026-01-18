83fb4ff49d135866af2ae05676d32038e474f526
Moana <missn.hadebe@gmail.com>
2026-01-18 12:19:25 +0200

    implement role-based access control with UserProfile, views, urls, and templates

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,4 +1,18 @@
 from django.urls import path
+from .views import admin_view, librarian_view, member_view
+
+urlpatterns = [
+    path('admin-role/', admin_view, name='admin_view'),
+    path('librarian-role/', librarian_view, name='librarian_view'),
+    path('member-role/', member_view, name='member_view'),
+]
+
+
+
+
+
+
+from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 from .views import list_books, LibraryDetailView, register_view
 
