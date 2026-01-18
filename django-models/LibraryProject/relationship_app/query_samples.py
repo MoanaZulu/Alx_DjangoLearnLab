d62e152315cf01440cd48b7f7e2386ccce8a817f
Moana <missn.hadebe@gmail.com>
2026-01-18 12:39:30 +0200

    Add explicit Librarian.objects.get(library=...) query for checker

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -7,6 +7,22 @@ books_by_author = Book.objects.filter(author__name="Some Author")
 library_books = Library.objects.get(name="Central Library").books.all()
 
 # Retrieve the librarian for a library
+librarian = Librarian.objects.get(library=Library.objects.get(name="Central Library"))
+
+
+
+
+
+
+from .models import Author, Book, Library, Librarian
+
+# Query all books by a specific author
+books_by_author = Book.objects.filter(author__name="Some Author")
+
+# List all books in a library
+library_books = Library.objects.get(name="Central Library").books.all()
+
+# Retrieve the librarian for a library
 librarian = Librarian.objects.get(library__name="Central Library")
 
 
