9abcbb0d8d6084eb483ae7e55e9fefc4313222de
Moana <missn.hadebe@gmail.com>
2026-01-18 12:28:44 +0200

    Add URL patterns for add_book,edit_book, and delete_book views

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,4 +1,25 @@
 from django.urls import path
+from .views import list_books, LibraryDetailView, add_book, edit_book, delete_book
+
+
+
+urlpatterns = [
+    # Existing routes
+    path('books/', list_books, name='list_books'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+
+    # Custom permission-secured routes
+    path('add_book/', add_book, name='add_book'),
+    path('edit_book/<int:pk>/', edit_book, name='edit_book'),
+    path('delete_book/<int:pk>/', delete_book, name='delete_book'),
+]
+
+
+
+
+
+
+from django.urls import path
 from .views import admin_view, librarian_view, member_view
 
 urlpatterns = [
