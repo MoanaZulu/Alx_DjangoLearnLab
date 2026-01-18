9a51013e026486d0df7ead701a01244056da24e2
Moana <missn.hadebe@gmail.com>
2026-01-18 08:28:06 +0200

    Fix views,urls, templates; remove stray relationships folder

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -74,3 +74,16 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
+
+
+def list_books(request):
+    books = Book.objects.all()   # <-- must be objects, not object
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+
+
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
