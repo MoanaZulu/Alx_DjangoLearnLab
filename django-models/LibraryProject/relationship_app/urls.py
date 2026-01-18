9f7bdf52a6b9910a27e2575d4ae46d4a66e98a69
Moana <missn.hadebe@gmail.com>
2026-01-18 11:27:04 +0200

    Use explicit imports for list_books and librarydetailView in urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,4 +1,19 @@
 from django.urls import path
+from .views import list_books, LibraryDetailView   # <-- explicit imports
+
+
+
+urlpatterns = [
+    path('books/', list_books, name='list_books'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+]
+
+
+
+
+
+
+from django.urls import path
 from . import views
 
 urlpatterns = [
