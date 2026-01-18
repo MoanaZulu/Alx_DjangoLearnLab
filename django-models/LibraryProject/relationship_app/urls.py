af7104a85d1c0eecda4b15655ee9195513e31ca5
Moana <missn.hadebe@gmail.com>
2026-01-18 11:07:50 +0200

    Fix views import, class-based view, urls, and templates

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,6 +2,19 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('books/', views.list_books, name='list_books'),
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+]
+
+
+
+
+
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
     path('books/add/', views.add_book, name='add_book'),
     path('books/<int:book_id>/edit/', views.edit_book, name='edit_book'),
     path('books/<int:book_id>/delete/', views.delete_book, name='delete_book'),
