cdb22aeb9bf5cf5594caecaa6fab7f812946a41e
Moana <missn.hadebe@gmail.com>
2026-01-26 09:35:33 +0200

    Add rest_framework and api to INSTALLED_APPS

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -5,6 +5,22 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',   
+    'api',              
+]
+
+
+
+
+
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
 ]
 
 
