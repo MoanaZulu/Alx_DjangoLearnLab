d2410577147aa3dfd296ab09ee75b1655c2503aa
Moana <missn.hadebe@gmail.com>
2026-02-08 14:58:39 +0200

    Updated BookListView to use filters.OrderingFilter for checker compliance

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,16 @@
+class BookListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = ["title", "author", "publication_year"]
+    search_fields = ["title", "author__name"]
+    ordering_fields = ["title", "publication_year"]
+    ordering = ["title"]  # default ordering
+
+
+
 from rest_framework import generics, filters
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from django_filters import rest_framework
