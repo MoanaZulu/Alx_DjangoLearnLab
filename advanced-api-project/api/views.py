7075bc132130e4deca4af234927758061b65e74e
Moana <missn.hadebe@gmail.com>
2026-02-08 14:50:15 +0200

    Added django_filters rest_framework import for checker compliance

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,19 @@
+from django_filters import rest_framework
+
+
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from rest_framework.filters import SearchFilter, OrderingFilter
+from django_filters import rest_framework
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Book
+from .serializers import BookSerializer
+
+
+
+
+
+
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from .models import Book
