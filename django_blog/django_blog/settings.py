e72e31e7d2f8a7bfa66838c84ac1c69249297124
Moana <missn.hadebe@gmail.com>
2026-02-14 16:45:25 +0200

    Implement Post model and confirm database config

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -1,3 +1,15 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
+    }
+}
+
+
+
+
+
+
 INSTALLED_APPS = [
     ...
     'blog',
