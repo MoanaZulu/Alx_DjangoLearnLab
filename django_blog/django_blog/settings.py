9e23bc934be3abab4e2a123f93342ce364c62779
Moana <missn.hadebe@gmail.com>
2026-02-14 16:25:20 +0200

    Register blog app in INSTALLED_APPS

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -5,6 +5,21 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',  # <-- must be spelled exactly like this
+]
+
+
+
+
+
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
     'blog',  # <-- add this line
 ]
 
