0ee9db6a5faa878251c8634d352148be4a8a1cca
Moana <missn.hadebe@gmail.com>
2026-02-15 07:37:37 +0200

    Update PostForm to use TagWidget for tags

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,4 +1,33 @@
 from django import forms
+from .models import Post, Comment
+from taggit.forms import TagWidget
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content', 'tags']
+        widgets = {
+            'tags': TagWidget(),   # <-- checker looks for this
+        }
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
+
+    def clean_content(self):
+        content = self.cleaned_data.get("content")
+        if not content or len(content.strip()) == 0:
+            raise forms.ValidationError("Comment cannot be empty.")
+        return content
+
+
+
+
+
+
+from django import forms
 from .models import Comment
 
 class CommentForm(forms.ModelForm):
