b3623093b0d5d27c7bbb666a1a2d1c6f1eda2730
Moana <missn.hadebe@gmail.com>
2026-02-15 07:46:57 +0200

    Add class-based views for Comment CRUD with LoginRequiredMixin and UserPassesTestMixin, plus templates

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,4 +1,18 @@
 from django.urls import path
+from .views import CommentCreateView, CommentUpdateView, CommentDeleteView
+
+urlpatterns = [
+    path("posts/<int:post_id>/comments/new/", CommentCreateView.as_view(), name="new_comment"),
+    path("comments/<int:pk>/edit/", CommentUpdateView.as_view(), name="edit_comment"),
+    path("comments/<int:pk>/delete/", CommentDeleteView.as_view(), name="delete_comment"),
+]
+
+
+
+
+
+
+from django.urls import path
 from .views import PostByTagListView
 
 urlpatterns = [
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,57 @@
+class CommentCreateView(LoginRequiredMixin, CreateView):
+    model = Comment
+    form_class = CommentForm
+    template_name = "blog/new_comment.html"
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        form.instance.post_id = self.kwargs["post_id"]
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy("post_detail", kwargs={"post_id": self.kwargs["post_id"]})
+
+
+class CommentUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Comment
+    form_class = CommentForm
+    template_name = "blog/edit_comment.html"
+
+    def test_func(self):
+        comment = self.get_object()
+        return comment.author == self.request.user
+
+    def get_success_url(self):
+        return reverse_lazy("post_detail", kwargs={"post_id": self.object.post.id})
+
+
+class CommentDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Comment
+    template_name = "blog/delete_comment.html"
+
+    def test_func(self):
+        comment = self.get_object()
+        return comment.author == self.request.user
+
+    def get_success_url(self):
+        return reverse_lazy("post_detail", kwargs={"post_id": self.object.post.id})
+
+
+
+
+
+
+from django.views.generic import CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.urls import reverse_lazy
+from .models import Comment
+from .forms import CommentForm
+
+
+
+
+
+
 from django.views.generic import ListView
 from .models import Post
 
