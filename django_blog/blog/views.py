5d4eb3b2f3c12e45439103c2c0631f793f233445
Moana <missn.hadebe@gmail.com>
2026-02-15 08:08:37 +0200

    Add login/register static files, profile view with POST handling, and CSRF protection

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,4 +1,32 @@
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django import forms
+
+class UserUpdateForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ["username", "email"]
+
+@login_required
+def profile(request):
+    if request.method == "POST":
+        form = UserUpdateForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Your profile has been updated!")
+            return redirect("profile")
+    else:
+        form = UserUpdateForm(instance=request.user)
+    return render(request, "blog/profile.html", {"form": form})
+
+
+
+
+
+
+from django.contrib.auth.decorators import login_required
 
 
 
