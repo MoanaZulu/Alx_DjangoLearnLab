eae8b5e72e596064495e564ba8ab861e721ee190
Moana <missn.hadebe@gmail.com>
2026-02-19 16:05:59 +0200

    Fix RegisterSerializer to use get_user_model and configure accounts URLs

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,3 +1,26 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+from django.contrib.auth import authenticate
+from rest_framework.authtoken.models import Token
+
+User = get_user_model()
+
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = get_user_model().objects.create_user(**validated_data)
+        Token.objects.create(user=user)
+        return user
+
+
+
+
+
+
 from rest_framework import serializers
 from django.contrib.auth import authenticate
 from rest_framework.authtoken.models import Token
