b3f972766e400a933b967034eddedc7c05c381ef
Moana <missn.hadebe@gmail.com>
2026-02-21 16:13:35 +0200

    Add Like model to posts app

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -1,6 +1,27 @@
 from django.db import models
 from django.conf import settings
 
+# existing Post and Comment models above...
+
+class Like(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='likes')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('post', 'user')  # prevents duplicate likes
+
+    def __str__(self):
+        return f"{self.user} liked {self.post}"
+
+
+
+
+
+
+from django.db import models
+from django.conf import settings
+
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='posts')
     title = models.CharField(max_length=255)
