ec2a1e7e4b80f3e32fe3c93f9ad358298712ad8f
Moana <missn.hadebe@gmail.com>
2026-02-21 12:15:17 +0200

    Fix accounts app registration in apps.py and settings.py

--- social_media_api/settings.py
+++ social_media_api/settings.py
@@ -1,6 +1,14 @@
 INSTALLED_APPS = [
-    ...,
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
     'accounts',
+    'posts',   # <-- add this once posts app is created
 ]
+
+AUTH_USER_MODEL = 'accounts.CustomUser'
