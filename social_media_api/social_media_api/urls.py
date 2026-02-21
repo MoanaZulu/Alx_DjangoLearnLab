bdbca8a6c5ddd42949eca45d52abc79b95baefb6
Moana <missn.hadebe@gmail.com>
2026-02-21 12:42:51 +0200

    Add posts URLs under api/ path in main urls.py

--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -1,3 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('posts.urls')),   # <-- add this line
+    path('api/accounts/', include('accounts.urls')),  # optional, if you want accounts under api too
+]
 
 
 
